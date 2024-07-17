4dd2194658c89d834681800dc3f0ea19154cd108
dpolygon <dannyg@lonestardev.net>
2024-07-17 14:43:46 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Gonzalez on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
